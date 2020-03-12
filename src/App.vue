6b0e4b850aa34e4b1a3f9dab3304dc3de3dca592
Peter Glennon <pcrglennon@gmail.com>
2020-03-12 14:52:29 -0400

    Set up tailwind/purgecss

--- src/App.vue
+++ src/App.vue
@@ -17,13 +17,4 @@ import HelloWorld from './components/HelloWorld.vue';
 export default class App extends Vue {}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./assets/tailwind.css">
